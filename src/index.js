1cf92d00a1bf7e2ab81f660338953518ca5bc2ba
smithwebtek <brad@smithwebtek.com>
2017-11-03 20:12:46 -0400

    first 2 components

--- src/index.js
+++ src/index.js
@@ -4,5 +4,34 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const MOUNT1 = document.querySelector('#section1')
+const MOUNT2 = document.querySelector('#section2')
+const MOUNT3 = document.querySelector('#section3')
+
+class Button extends React.Component {
+  render() {
+    return React.createElement('button', {}, 'Click me!');
+  }
+};
+
+class ShoppingList extends React.Component {
+  render() {
+    return React.createElement('div', {},[ 
+    React.createElement('h4', {}, "My List"),
+      React.createElement('ul', {}, 
+      [React.createElement('li', {}, 'Bananas'),
+        React.createElement('li', {}, 'Vanilla ice cream'),
+        React.createElement('li', {}, 'Chocolate')
+      ])
+    ])
+  }
+}
+
+const MyStuff = React.createElement('div', {}, Button, ShoppingList)
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(<Button />, MOUNT1);
+ReactDOM.render(<ShoppingList />, MOUNT2);
