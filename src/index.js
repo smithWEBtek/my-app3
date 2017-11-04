013228f9770a51326b0da214de0b35b506487136
smithwebtek <brad@smithwebtek.com>
2017-11-03 21:06:12 -0400

    rendering functions, consts, and classes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 const MOUNT1 = document.querySelector('#section1')
 const MOUNT2 = document.querySelector('#section2')
 const MOUNT3 = document.querySelector('#section3')
+const MOUNT4 = document.querySelector('#section4')
+const MOUNT5 = document.querySelector('#section5')
+
+const fruits = ["mango", "apple", "grape", "bananna", "plum", "durian"]
 
 class Button extends React.Component {
   render() {
@@ -27,11 +31,35 @@ class ShoppingList extends React.Component {
   }
 }
 
+const nestedButtons = React.createElement('div', {}, [
+  React.createElement(Button),
+  React.createElement(Button),
+]);
+
+function myFruits(){
+  let fruitList = []
+  for(let i=0;i<fruits.length;i++){
+    fruitList.push(React.createElement('li', {}, fruits[i]))
+  }
+  return fruitList
+}
+
+class MyFruitList extends React.Component {
+  render() {
+    return React.createElement('div', {},
+      [React.createElement('h4', {}, "My Fruits"),
+      React.createElement('ul', {}, myFruits())
+    ])
+  }
+}
+
 const MyStuff = React.createElement('div', {}, Button, ShoppingList)
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
 
 ReactDOM.render(<Button />, MOUNT1);
 ReactDOM.render(<ShoppingList />, MOUNT2);
+ReactDOM.render(myFruits(), MOUNT3);
+ReactDOM.render(<MyFruitList />, MOUNT4);
+ReactDOM.render(nestedButtons, MOUNT5);
